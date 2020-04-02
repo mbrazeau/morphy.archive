c9411579ea1fcb63c0d8248f5f50efc4830df196
Martin Brazeau <martin.brazeau@gmail.com>
2020-04-02 13:25:20 +0100

    add update argument

--- getdev.py
+++ getdev.py
@@ -8,14 +8,30 @@
 import  sys, os, getopt, multiprocessing, platform, shutil
 from subprocess import call
 
-def main(argv):
-    print("Fetching all Morphy subprojects.")
-    os.system("git pull")
+def pullAll():
+    os.system("git -C ./mpl pull")
+    os.system("git -C ./nui pull")
+
+def cloneAllDev():
     os.system("git clone --branch development https://github.com/mbrazeau/mpl.git ./mpl/")
     os.system("git clone --branch dev https://github.com/mbrazeau/nui.git ./nui/")
     os.system("git clone --branch dev https://github.com/mbrazeau/buildMorphy.git ./build/")
     os.system("git clone https://github.com/cdesjardins/wineditline.git ./external/wineditline")
     os.system("git clone https://github.com/mtholder/ncl.git ./external/ncl/")
+
+def main(argv):
+
+    args = ["update"]
+
+    print("Fetching all Morphy subprojects.")
+    
+    if (len(argv) > 0):
+        if (argv[0] == "update"):
+            pullAll()
+        else:
+            print("Unrecognised argument")
+    else:
+        cloneAllDev()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
